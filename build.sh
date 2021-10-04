7165f1a41c55745a714ebdc59b7ace9ca60f5901
dcs0spb <s.p.bradley@durham.ac.uk>
2021-10-04 22:36:14 +0100

    Working on presentation progblack_lectures/comp1011_intro

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sed '/^---/ d' PITCHME.md | sed '/^@/ d' | sed '/^:::/ d' | sed 's/{.*}//' > README.md
+
+PATHEND=$(pwd | rev | cut -d'/' -f-2 | rev)
+
+pandoc  -V theme=simple -t revealjs -s PITCHME.md -o PITCHME.html
+
+git add .
+git commit -m "Working on presentation $PATHEND"
+git push -u origin master
+
