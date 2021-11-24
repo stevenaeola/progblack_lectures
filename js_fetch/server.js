4f3564ecccde19e6e90acfe3c729d464e3ab6db7
dcs0spb <s.p.bradley@durham.ac.uk>
2021-11-24 20:40:04 +0000

    Working on presentation progblack_lectures/js_fetch

--- js_fetch/server.js
+++ js_fetch/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('client'));
-
 let instruments = [ 'piano', 'concertina', 'double bass'];
 
 app.get('/list', function (req, resp){
