3d7119d00b9a320cff0ccf54f34390f495908134
dcs0spb <s.p.bradley@durham.ac.uk>
2021-11-25 22:56:07 +0000

    Update from lecture 25/11/2021

--- js_fetch/server.js
+++ js_fetch/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('client'));
+
 let instruments = [ 'piano', 'concertina', 'double bass'];
 
 app.get('/list', function (req, resp){
